dd954918cb749e93cb98bdb6dd355dbd34adf5d0
CHawkins92 <conor.hawkins@gmx.com>
2021-10-15 08:21:13 +0000

    Initialized project:
    - Spring WEB
    - Spring JPA
    - H2 Database
    - Lombok

---
+++ src/main/java/com/example/solobackend/SolobackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.solobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolobackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solobackend/SolobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolobackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

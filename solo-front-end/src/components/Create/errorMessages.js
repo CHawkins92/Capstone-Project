00bd9d5944c61b44dd2b374f7f091f54e0691898
CHawkins92 <conor.hawkins@gmx.com>
2021-12-01 14:51:41 +0000

    Modified error message for additional drivers in errorMessages.js

--- solo-front-end/src/components/Create/errorMessages.js
+++ solo-front-end/src/components/Create/errorMessages.js
@@ -21,6 +21,6 @@ export const POSTCODE_INVALID = "Postcode is invalid";
 export const DATE_REGISTERED_INVALID =
   "Date of Registration can not be a future date";
 export const ADDITIONAL_DRIVERS_INVALID =
-  "Please enter a value in range 0-5";
+  "Please enter a value in range 0-4";
 export const CURRENT_VALUE_INVALID =
     "Please enter a value in range £0-50000";
